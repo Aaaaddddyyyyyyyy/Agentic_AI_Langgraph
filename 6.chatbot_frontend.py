08efa19865358ad62b27a57d2cbc44615fc5a232
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-08-07 08:23:29 +0530

    chatbot

--- 6.chatbot_frontend.py
+++ 6.chatbot_frontend.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from 5.chatbot import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG = {'configurable' : {'thread_id': 'thread1'}}
 
 
 # session state -> dict-> not erasing the history of particular session
@@ -35,7 +39,9 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    st.session_state['message_history'].append({'role':'assistant','content': user_input})
+    response= chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
+    ai_message=response['messages'][-1].content
+    st.session_state['message_history'].append({'role':'assistant','content': ai_message})
     with st.chat_message('assistant'):
-            st.text(user_input)
-# chat history storage
+            st.text(ai_message)
+ 
