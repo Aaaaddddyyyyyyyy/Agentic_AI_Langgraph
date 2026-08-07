f1a6b026cd578d659aa0e966b5e103df55adf651
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-08-07 08:06:40 +0530

    frontend_chatbot

--- 6.chatbot_frontend.py
+++ 6.chatbot_frontend.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
-message_history=[]
 
-for message in message_history:
+# session state -> dict-> not erasing the history of particular session
+if 'message_history' not in st.session_state:
+     st.session_state['message_history']=[]
+
+
+# loading conversation history
+for message in st.session_state['message_history']:
      with st.chat_message(message['role']):
           st.text(message['content'])
 
@@ -26,11 +31,11 @@ user_input=st.chat_input('Type your message here')
 if user_input:
 
     # first add message to message_history
-    message_history.append({'role':'user','content': user_input})
+    st.session_state['message_history'].append({'role':'user','content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    message_history.append({'role':'assistant','content': user_input})
+    st.session_state['message_history'].append({'role':'assistant','content': user_input})
     with st.chat_message('assistant'):
             st.text(user_input)
 # chat history storage
