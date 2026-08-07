f52a67778e91a56bbab8c72aa683438c33683664
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-08-07 20:22:25 +0530

    chatbot_stream_integration

--- chatbot_mem.py
+++ chatbot_mem.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
@@ -31,3 +31,12 @@ graph.add_edge("chat_node", END)
 checkpointer = MemorySaver()
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+stream = chatbot.stream(
+    {'messages':[HumanMessage(content='what is the reciepe to make pasta?')]},
+    config = {'configurable' : {'thread_id': 'thread1'}},
+    stream_mode='messages'
+
+)
+
+print(type(stream))
