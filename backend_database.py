7a47109f3bbce60787ce7bb3daa5610c4f90c436
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-08-08 07:10:33 +0530

    backend_Database

---
+++ backend_database.py
@@ -0,0 +1,43 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+checkpointer = MemorySaver()
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+for message_chunk,metadata in chatbot.stream(
+    {'messages':[HumanMessage(content='what is the reciepe to make pasta?')]},
+    config = {'configurable' : {'thread_id': 'thread1'}},
+    stream_mode='messages'
+
+):
+    if message_chunk.content:
+        print(message_chunk.content,end=" ",flush=True)
