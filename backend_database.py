1ce4c8279b82b73f6b24c50a032f5abe38a05831
Aaaaddddyyyyyyyy <iadityajha693@gmail.com>
2026-08-08 07:12:59 +0530

    backend_Database

--- backend_database.py
+++ backend_database.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
 from langgraph.graph.message import add_messages
+import sqlite3
 
 load_dotenv()
 
@@ -28,7 +29,10 @@ graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-checkpointer = MemorySaver()
+
+
+
+checkpointer = SqliteSaver()
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
